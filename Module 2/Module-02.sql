896ef2a314ca92b256d341c6f1ac36ce6688dc94
royaad <royaad@live.com>
2023-12-19 09:45:56 +0200

    module 2 initial commit

---
+++ Module 2/Module-02.sql
@@ -0,0 +1,14 @@
+/*
+Terminology
+Entities map to tables
+Attributes map to columns in a table
+Data types
+varchar()	# for characters variable in length
+char 		# if they do not vary in length
+bigint		# hold a number up to 19 digits long
+
+Each table is assigned a primary key which uniquely identifies each row.
+Table can also contain foreign keys which correspond to primary keys of other tables therefore linking them together.
+860cc263f1d11f35940a5a9fecb581ff
+*/
+
