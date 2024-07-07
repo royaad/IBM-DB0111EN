a15458c950055f1cae41019f614b2b1396cd0bf5
royaad <royaad@live.com>
2024-07-07 18:04:29 +0300

    last update before work.

--- Basics.sql
+++ Basics.sql
@@ -1,20 +1,22 @@
--- if we want to access the table in a schema, we can use the . attribute operator
+# If we want to access the table in a schema, we can use the . attribute operator
 SELECT * FROM pets.petrescue;
--- create a new schema
+# Create a new schema
 CREATE SCHEMA `chicago_schools`;
--- show list of tables in schema
+# Show tables in currently active schema
 SHOW TABLES;
--- show column names and types
+# show list of tables in specific schema
+SHOW TABLES IN chicago_schools;
+# show column names and types
 SHOW COLUMNS FROM chicago_public_schools;
--- only for NAME_OF_SCHOOL /  the '' are necessary
+# only for NAME_OF_SCHOOL /  the '' are necessary
 SHOW COLUMNS FROM chicago_public_schools where field = 'NAME_OF_SCHOOL';
--- or use desc or describe function
+# or use desc or describe function
 DESC chicago_public_schools;
--- just for NAME_OF_SCHOOL
+# just for NAME_OF_SCHOOL
 DESC chicago_public_schools NAME_OF_SCHOOL;
--- any columns containing school
+# any columns containing school
 DESC chicago_public_schools '%SCHOOL%';
--- use INFORMATION SCHEMA
+# use INFORMATION SCHEMA
 SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH AS MAX_LENGTH, CHARACTER_OCTET_LENGTH AS OCTET_LENGTH 
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = DATABASE()
